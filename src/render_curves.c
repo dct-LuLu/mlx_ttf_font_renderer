8fae17d4dcecb2efa11f166a0198dba5f43f4189
dct-LuLu <69580109+dct-LuLu@users.noreply.github.com>
2025-05-27 17:28:21 +0200

    refactored some code need to fix scaling issue on Y axis and full round curves not displaying at all

--- src/render_curves.c
+++ src/render_curves.c
@@ -3,94 +3,121 @@
 /*                                                        :::      ::::::::   */
 /*   render_curves.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/26 15:27:24 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/05/26 18:43:48 by jaubry--         ###   ########lyon.fr   */
+/*   Created: 2025/05/27 16:41:38 by jaubry--          #+#    #+#             */
+/*   Updated: 2025/05/27 16:41:47 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "font_renderer.h"
 
 /**
- * @brief Draw curve sequence starting from an on-curve point
+ * @brief Get next index in contour with wrap-around
  */
-void	draw_curve_from_on_curve(t_env *env, t_glyf_table *glyph, int curr_idx,
-		int ctr_start_idx, int ctr_end_idx, t_vec2 pos, int color,
-		t_glyf_component *transform)
+static int	get_next_contour_idx(int curr_idx, int start_idx, int end_idx)
 {
-	t_vec2	next_control;
+	if (curr_idx == end_idx)
+		return (start_idx);
+	return (curr_idx + 1);
+}
+
+/**
+ * @brief Get transformed point from glyph coordinates
+ */
+static t_vec2	get_transformed_point(t_glyf_table *glyph, int point_idx,
+					t_glyf_component *transform)
+{
+	t_vec2	point_pt;
+
+	point_pt = new_vec2(glyph->x_coordinates[point_idx],
+			glyph->y_coordinates[point_idx]);
+	return (apply_transform(point_pt, transform));
+}
+
+/**
+ * @brief Create implied on-curve point between two off-curve points
+ */
+static t_vec2	create_implied_point(t_vec2 control1_pt, t_vec2 control2_pt)
+{
+	t_vec2	implied_pt;
+
+	implied_pt.x = (control1_pt.x + control2_pt.x) / 2;
+	implied_pt.y = (control1_pt.y + control2_pt.y) / 2;
+	return (implied_pt);
+}
+
+/**
+ * @brief Draw single quadratic curve segment
+ */
+static void	draw_curve_segment(t_env *env, t_vec2 start_pt, t_vec2 control_pt,
+				t_vec2 end_pt, t_curve_params *params)
+{
 	t_vec2	screen_start;
 	t_vec2	screen_control;
 	t_vec2	screen_end;
 
-	int next_idx;
-	t_vec2 start_pt, end_pt, control_pt, implied_pt;
-	start_pt = new_vec2(glyph->x_coordinates[curr_idx],
-			glyph->y_coordinates[curr_idx]);
-	start_pt = apply_transform(start_pt, transform);
-	// Move to first off-curve point
-	curr_idx = (curr_idx == ctr_end_idx) ? ctr_start_idx : curr_idx
-		+ 1;
-	while (!(glyph->flags[curr_idx] & ON_CURVE))
+	screen_start = transform_coordinate(env, start_pt, params->pos);
+	screen_control = transform_coordinate(env, control_pt, params->pos);
+	screen_end = transform_coordinate(env, end_pt, params->pos);
+	ft_mlx_draw_quadratic_curve(&env->mlx->img, screen_start,
+		screen_control, screen_end, params->color);
+}
+
+/**
+ * @brief Determine end point for curve (on-curve or implied)
+ */
+static t_vec2	get_curve_end_point(t_glyf_table *glyph, t_vec2 control_pt,
+				int next_idx, t_glyf_component *transform)
+{
+	t_vec2	next_control_pt;
+
+	if (glyph->flags[next_idx] & ON_CURVE)
+		return (get_transformed_point(glyph, next_idx, transform));
+	next_control_pt = get_transformed_point(glyph, next_idx, transform);
+	return (create_implied_point(control_pt, next_control_pt));
+}
+
+/**
+ * @brief Process single curve iteration in sequence
+ */
+static int	process_curve_iteration(t_glyf_table *glyph, t_curve_state *state,
+				t_curve_params *params, t_env *env)
+{
+	t_vec2	control_pt;
+	t_vec2	end_pt;
+
+	control_pt = get_transformed_point(glyph, state->curr_idx,
+			params->transform);
+	state->next_idx = get_next_contour_idx(state->curr_idx,
+			state->start_idx, state->end_idx);
+	end_pt = get_curve_end_point(glyph, control_pt, state->next_idx,
+			params->transform);
+	draw_curve_segment(env, state->start_pt, control_pt, end_pt, params);
+	state->start_pt = end_pt;
+	state->curr_idx = state->next_idx;
+	return (glyph->flags[state->next_idx] & ON_CURVE);
+}
+
+/**
+ * @brief Draw curve sequence starting from an on-curve point
+ */
+void	draw_curve_from_on_curve(t_env *env, t_glyf_table *glyph,
+			int start_idx, t_curve_params *params)
+{
+	t_curve_state	state;
+
+	state.curr_idx = start_idx;
+	state.start_idx = params->contour_start;
+	state.end_idx = params->contour_end;
+	state.start_pt = get_transformed_point(glyph, state.curr_idx,
+			params->transform);
+	state.curr_idx = get_next_contour_idx(state.curr_idx,
+			state.start_idx, state.end_idx);
+	while (!(glyph->flags[state.curr_idx] & ON_CURVE))
 	{
-		control_pt = new_vec2(glyph->x_coordinates[curr_idx],
-				glyph->y_coordinates[curr_idx]);
-		control_pt = apply_transform(control_pt, transform);
-		next_idx = (curr_idx == ctr_end_idx) ? ctr_start_idx : curr_idx + 1;
-		if (glyph->flags[next_idx] & ON_CURVE)
-		{
-			// Next point is on-curve: draw curve to it
-			end_pt = new_vec2(glyph->x_coordinates[next_idx],
-					glyph->y_coordinates[next_idx]);
-			end_pt = apply_transform(end_pt, transform);
-		}
-		else
-		{
-			// Next point is also off-curve: create implied on-curve point
-			next_control = new_vec2(glyph->x_coordinates[next_idx],
-					glyph->y_coordinates[next_idx]);
-			next_control = apply_transform(next_control, transform);
-			implied_pt.x = (control_pt.x + next_control.x) / 2;
-			implied_pt.y = (control_pt.y + next_control.y) / 2;
-			end_pt = implied_pt;
-		}
-		// Draw the quadratic curve
-		screen_start = transform_coordinate(env, start_pt, pos);
-		screen_control = transform_coordinate(env, control_pt, pos);
-		screen_end = transform_coordinate(env, end_pt, pos);
-		ft_mlx_draw_quadratic_curve(&env->mlx->img, screen_start,
-			screen_control, screen_end, color);
-		// Move to next segment
-		start_pt = end_pt;
-		curr_idx = next_idx;
-		// If we've reached an on-curve point, we're done
-		if (glyph->flags[curr_idx] & ON_CURVE)
+		if (process_curve_iteration(glyph, &state, params, env))
 			break ;
 	}
 }
 
-/**
- * @brief Simplified curve sequence function (kept for compatibility)
- */
-void	draw_curve_sequence(t_env *env, t_glyf_table *glyph, int start_idx,
-		int end_idx, int ctr_start_idx, int ctr_end_idx, t_vec2 pos, int color,
-		t_glyf_component *transform)
-{
-	(void)end_idx;
-	int	prev_on_curve;
-
-	// Find the previous on-curve point
-	prev_on_curve = start_idx - 1;
-	if (prev_on_curve < ctr_start_idx)
-		prev_on_curve = ctr_end_idx;
-	while (!(glyph->flags[prev_on_curve] & ON_CURVE)
-		&& prev_on_curve != start_idx)
-	{
-		prev_on_curve--;
-		if (prev_on_curve < ctr_start_idx)
-			prev_on_curve = ctr_end_idx;
-	}
-	draw_curve_from_on_curve(env, glyph, prev_on_curve, ctr_start_idx,
-		ctr_end_idx, pos, color, transform);
-}
