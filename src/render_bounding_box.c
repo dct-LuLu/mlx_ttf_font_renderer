9c2c10cc3b1b0a88aaec72a5f815bcd723982b89
Jaurel Aubry--Richard <jaubry--@student.42lyon.fr>
2025-06-10 20:59:08 +0200

    tmp changes

--- src/render_bounding_box.c
+++ src/render_bounding_box.c
@@ -6,89 +6,72 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 13:45:29 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/05/28 02:24:48 by jaubry--         ###   ########lyon.fr   */
+/*   Updated: 2025/06/10 20:49:35 by jaubry--         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "font_renderer.h"
 
-/**
- * @brief Calculate transformed bounding box
- */
-static void	get_transformed_bounds(t_glyf_header *header,
-		t_glyf_component *comp, t_vec2 *min, t_vec2 *max)
+/*
+t_vec2	new_screen_pt(t_params *params, int x, int y, t_glyf_component *comp)
 {
-	t_vec2	corners[4];
+	t_vec2	pt;
 	t_vec2	transformed;
-	int		i;
-
-	if (!comp || !(comp->flags & (HAS_SCALE | HAS_XY_SCALE | HAS_2X2_MATRIX)))
-	{
-		*min = new_vec2(header->x_min, header->y_min);
-		*max = new_vec2(header->x_max, header->y_max);
-		return ;
-	}
-	// Get all four corners of original bounding box
-	corners[0] = new_vec2(header->x_min, header->y_min);
-	corners[1] = new_vec2(header->x_max, header->y_min);
-	corners[2] = new_vec2(header->x_min, header->y_max);
-	corners[3] = new_vec2(header->x_max, header->y_max);
-	// Transform all corners and find new bounds
-	*min = new_vec2(32767, 32767);
-	*max = new_vec2(-32768, -32768);
-	i = 0;
-	while (i < 4)
-	{
-		transformed = apply_transform(corners[i], comp);
-		if (transformed.x < min->x)
-			min->x = transformed.x;
-		if (transformed.y < min->y)
-			min->y = transformed.y;
-		if (transformed.x > max->x)
-			max->x = transformed.x;
-		if (transformed.y > max->y)
-			max->y = transformed.y;
-		i++;
-	}
+	t_vec2	screen;
+	
+	pt = new_vec2(x, y);
+	if (comp)
+	transformed = apply_transform(pt, comp);
+	screen = transform_coordinate(params->env, transformed, params->pos);
+	return (screen);
 }
+*/
 
-/**
- * @brief Draw bounding box with optional transformation
- */
-void	draw_transformed_bounding_box(t_env *env, t_glyf_header *header,
-		t_vec2 pos, int color, t_glyf_component *comp)
+void	draw_transformed_bounding_box(t_contour *contour, int scolor)
 {
-	t_vec2 min, max;
-	t_vec2 lb, rb, lt, rt;
-	get_transformed_bounds(header, comp, &min, &max);
-	lb = transform_coordinate(env, min, pos);
-	rt = transform_coordinate(env, max, pos);
-	rb = transform_coordinate(env, new_vec2(max.x, min.y), pos);
-	lt = transform_coordinate(env, new_vec2(min.x, max.y), pos);
-	ft_mlx_line_put(&env->mlx->img, lb, rb, color);
-	ft_mlx_line_put(&env->mlx->img, lb, lt, color);
-	ft_mlx_line_put(&env->mlx->img, rt, rb, color);
-	ft_mlx_line_put(&env->mlx->img, rt, lt, color);
+	t_vec2			corners[4];
+	t_env			*env;
+	t_glyf_header	*header;
+	t_vec2			pos;
+	int				color;
+
+	env = contour->env;
+	header = contour->glyf->header;
+	pos = contour->pos;
+	if (scolor == -1)
+		color = contour->color;
+	else
+		color = scolor;
+	corners[0] = transform_coordinate(env, new_vec2(header->x_min, header->y_min), pos);
+	corners[1] = transform_coordinate(env, new_vec2(header->x_max, header->y_max), pos);
+	corners[2] = transform_coordinate(env, new_vec2(header->x_max, header->y_min), pos);
+	corners[3] = transform_coordinate(env, new_vec2(header->x_min, header->y_max), pos);
+	ft_mlx_line_put(&env->mlx->img, corners[0], corners[2], color);
+	ft_mlx_line_put(&env->mlx->img, corners[0], corners[3], color);
+	ft_mlx_line_put(&env->mlx->img, corners[1], corners[2], color);
+	ft_mlx_line_put(&env->mlx->img, corners[1], corners[3], color);
 }
 
 /**
  * @brief Draw the font-wide max bounding box
  */
-void	draw_max_bounding_box(t_env *env, t_vec2 pos, int color)
+void	draw_max_bounding_box(t_contour *contour, int color)
 {
-	t_vec2			lb;
-	t_vec2			rb;
-	t_vec2			lt;
-	t_vec2			rt;
+	t_vec2			corners[4];
+	t_env			*env;
 	t_head_table	*head;
+	t_vec2			pos;
+	int				color;
 
+	env = contour->env;
 	head = env->font->head;
-	lb = transform_coordinate(env, new_vec2(head->x_min, head->y_min), pos);
-	rt = transform_coordinate(env, new_vec2(head->x_max, head->y_max), pos);
-	rb = transform_coordinate(env, new_vec2(head->x_max, head->y_min), pos);
-	lt = transform_coordinate(env, new_vec2(head->x_min, head->y_max), pos);
-	ft_mlx_line_put(&env->mlx->img, lb, rb, color);
-	ft_mlx_line_put(&env->mlx->img, lb, lt, color);
-	ft_mlx_line_put(&env->mlx->img, rt, rb, color);
-	ft_mlx_line_put(&env->mlx->img, rt, lt, color);
+	pos = contour->pos;
+	corners[0] = transform_coordinate(env, new_vec2(head->x_min, head->y_min), pos);
+	corners[1] = transform_coordinate(env, new_vec2(head->x_max, head->y_max), pos);
+	corners[2] = transform_coordinate(env, new_vec2(head->x_max, head->y_min), pos);
+	corners[3] = transform_coordinate(env, new_vec2(head->x_min, head->y_max), pos);
+	ft_mlx_line_put(&env->mlx->img, corners[0], corners[2], color);
+	ft_mlx_line_put(&env->mlx->img, corners[0], corners[3], color);
+	ft_mlx_line_put(&env->mlx->img, corners[1], corners[2], color);
+	ft_mlx_line_put(&env->mlx->img, corners[1], corners[3], color);
 }
--- src/render_glyph.c
+++ src/render_glyph.c
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 16:51:42 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/05/28 02:24:55 by jaubry--         ###   ########lyon.fr   */
+/*   Updated: 2025/06/10 20:56:26 by jaubry--         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@
 /**
  * @brief Initialize curve parameters structure
  */
-static t_curve_params	init_curve_params(t_vec2 pos, int color, int start_idx,
-		int end_idx, t_glyf_component *transform)
+static t_curve_params	init_curve_params(t_contour *contour, int start_idx,
+		int end_idx)
 {
 	t_curve_params	params;
 
-	params.pos = pos;
-	params.color = color;
 	params.contour_start = start_idx;
 	params.contour_end = end_idx;
-	params.transform = transform;
 	return (params);
 }
 
 /**
  * @brief Draw straight line between two on-curve points
  */
-static void	draw_straight_segment(t_env *env, t_glyf_table *glyph, int curr_idx,
+static void	draw_straight_segment(t_contour *contour, int curr_idx,
 		int next_idx, t_curve_params *params)
 {
 	t_vec2	curr_pt;
@@ -39,58 +36,60 @@ static void	draw_straight_segment(t_env *env, t_glyf_table *glyph, int curr_idx,
 	t_vec2	screen_curr;
 	t_vec2	screen_next;
 
-	curr_pt = new_vec2(glyph->x_coordinates[curr_idx],
-			glyph->y_coordinates[curr_idx]);
-	next_pt = new_vec2(glyph->x_coordinates[next_idx],
-			glyph->y_coordinates[next_idx]);
-	curr_pt = apply_transform(curr_pt, params->transform);
-	next_pt = apply_transform(next_pt, params->transform);
-	screen_curr = transform_coordinate(env, curr_pt, params->pos);
-	screen_next = transform_coordinate(env, next_pt, params->pos);
-	ft_mlx_line_put(&env->mlx->img, screen_curr, screen_next, params->color);
+	curr_pt = new_vec2(contour->glyf->x_coordinates[curr_idx],
+			contour->glyf->y_coordinates[curr_idx]);
+	next_pt = new_vec2(contour->glyf->x_coordinates[next_idx],
+			contour->glyf->y_coordinates[next_idx]);
+	curr_pt = apply_transform(curr_pt, contour->transform);
+	next_pt = apply_transform(next_pt, contour->transform);
+	screen_curr = transform_coordinate(contour->env, curr_pt, contour->pos);
+	screen_next = transform_coordinate(contour->env, next_pt, contour->pos);
+	ft_mlx_line_put(&contour->env->mlx->img, screen_curr, screen_next, contour->color);
 }
 
 /**
  * @brief Process single point in contour
  */
-static void	process_contour_point(t_env *env, t_glyf_table *glyph, int curr_idx,
+static void	process_contour_point(t_contour *contour, int curr_idx,
 		t_curve_params *params)
 {
 	int	next_idx;
 
-	if (!(glyph->flags[curr_idx] & ON_CURVE))
+	if (!(contour->glyf->flags[curr_idx] & ON_CURVE))
 		return ;
-	next_idx = (curr_idx == params->contour_end) ? params->contour_start : curr_idx
-		+ 1;
-	if (glyph->flags[next_idx] & ON_CURVE)
-		draw_straight_segment(env, glyph, curr_idx, next_idx, params);
+	next_idx = params->contour_start;
+	if (curr_idx != params->contour_end)
+		next_idx = curr_idx + 1;
+	if (contour->glyf->flags[next_idx] & ON_CURVE)
+		draw_straight_segment(contour, curr_idx, next_idx, params);
 	else
-		draw_curve_from_on_curve(env, glyph, curr_idx, params);
+		draw_curve_from_on_curve(contour->env, contour->glyf, curr_idx, params);
 }
 
 /**
  * @brief Draw a single contour with optional transformation
  */
-static void	draw_contour(t_env *env, t_glyf_table *glyph, int contour_idx,
-		t_vec2 pos, int color, t_glyf_component *transform)
+static void	draw_contour(t_contour *contour)
 {
 	int				curr_idx;
 	int				start_idx;
 	int				end_idx;
 	t_curve_params	params;
 
-	start_idx = (contour_idx == 0) ? 0 : glyph->end_pts[contour_idx - 1] + 1;
-	end_idx = glyph->end_pts[contour_idx];
-	params = init_curve_params(pos, color, start_idx, end_idx, transform);
-	if (!has_on_curve_points(glyph, start_idx, end_idx))
+	start_idx = 0;
+	if (contour->idx != 0)
+		start_idx = contour->glyf->end_pts[contour->idx - 1] + 1;
+	end_idx = contour->glyf->end_pts[contour->idx];
+	params = init_curve_params(contour, start_idx, end_idx);
+	if (!has_on_curve_points(contour->glyf, start_idx, end_idx))
 	{
-		draw_all_off_curve_contour(env, glyph, &params);
+		draw_all_off_curve_contour(contour, &params);
 		return ;
 	}
 	curr_idx = start_idx;
 	while (curr_idx <= end_idx)
 	{
-		process_contour_point(env, glyph, curr_idx, &params);
+		process_contour_point(contour, curr_idx, &params);
 		curr_idx++;
 	}
 }
@@ -98,51 +97,41 @@ static void	draw_contour(t_env *env, t_glyf_table *glyph, int contour_idx,
 /**
  * @brief Draw a simple glyph with optional transformation
  */
-static void	draw_simple_glyph(t_env *env, t_glyf_table *glyph, t_vec2 pos,
-		int color, t_glyf_component *transform)
+static void	draw_simple_glyph(t_contour *contour)
 {
-	int	contour_idx;
-
-	if (!glyph || glyph->header->number_of_contours <= 0)
+	if (!contour->glyf || contour->glyf->header->number_of_contours <= 0)
 		return ;
-	contour_idx = 0;
-	while (contour_idx < glyph->header->number_of_contours)
+	contour->idx = 0;
+	while (contour->idx < contour->glyf->header->number_of_contours)
 	{
-		draw_contour(env, glyph, contour_idx, pos, color, transform);
-		contour_idx++;
+		draw_contour(contour);
+		contour->idx++;
 	}
 	if (DEBUG)
-		draw_transformed_bounding_box(env, glyph->header, pos, YELLOW,
-			transform);
+		draw_transformed_bounding_box(contour, YELLOW);
 }
 
 /**
  * @brief Draw a composite glyph by rendering all components
  */
-static void	draw_composite_glyph(t_env *env, t_glyf_table *glyph, t_vec2 pos,
-		int color)
+static void	draw_composite_glyph(t_contour *contour)
 {
 	t_glyf_component	*comp;
-	t_glyf_table		*comp_glyph;
-	t_vec2				comp_pos;
 
-	if (!glyph || !glyph->component)
+	if (!contour->glyf || !contour->glyf->component)
 		return ;
-	comp = glyph->component;
+	comp = contour->glyf->component;
 	while (comp)
 	{
-		if (comp->glyph_index >= env->font->maxp->num_glyphs
-			|| !env->font->glyfs[comp->glyph_index])
+		if (!(comp->glyph_index >= contour->env->font->maxp->num_glyphs
+			|| !contour->env->font->glyfs[comp->glyph_index]))
 		{
-			comp = comp->next;
-			continue ;
+			contour->glyf = contour->env->font->glyfs[comp->glyph_index];
+			contour->pos = get_component_position(contour->pos, comp);
+			contour->transform = comp;
+			if (contour->glyf->header->number_of_contours >= 0)
+				draw_simple_glyph(contour);
 		}
-		comp_glyph = env->font->glyfs[comp->glyph_index];
-		comp_pos = get_component_position(pos, comp);
-		if (comp_glyph->header->number_of_contours >= 0)
-			draw_simple_glyph(env, comp_glyph, comp_pos, color, comp);
-		else if (comp_glyph->header->number_of_contours == -1)
-			draw_glyph_outline(env, comp->glyph_index, comp_pos, color);
 		comp = comp->next;
 	}
 }
@@ -150,19 +139,19 @@ static void	draw_composite_glyph(t_env *env, t_glyf_table *glyph, t_vec2 pos,
 /**
  * @brief Draw any glyph (simple or composite) with automatic detection
  */
-void	draw_glyph_outline(t_env *env, size_t glyph_idx, t_vec2 pos, int color)
+void	draw_glyph_outline(t_contour *contour)
 {
 	t_glyf_table	*glyph;
 
-	if (glyph_idx >= env->font->maxp->num_glyphs)
+	if (contour->idx >= contour->env->font->maxp->num_glyphs)
 		return ;
-	glyph = env->font->glyfs[glyph_idx];
+	glyph = contour->env->font->glyfs[contour->idx];
 	if (!glyph)
 		return ;
-	if (DEBUG && (glyph->header->number_of_contours >= -1))
-		draw_transformed_bounding_box(env, glyph->header, pos, GREEN, NULL);
-	if (glyph->header->number_of_contours >= 0)
-		draw_simple_glyph(env, glyph, pos, color, NULL);
-	else if (glyph->header->number_of_contours == -1)
-		draw_composite_glyph(env, glyph, pos, color);
+	if (DEBUG)
+		draw_transformed_bounding_box(contour, GREEN);
+	if (glyph->header->number_of_contours == -1)
+		draw_composite_glyph(contour);
+	else if (glyph->header->number_of_contours >= 0)
+		draw_simple_glyph(contour);
 }
