d6d185f2acdd15c60a68ecb7bdcdff1acac43853
Jaurel Aubry--Richard <jaubry--@student.42lyon.fr>
2025-08-07 03:49:09 +0200

    DELETE: removed time_utils in favor in libft's

--- src/utils/utils.mk
+++ src/utils/utils.mk
@@ -6,7 +6,7 @@
 #    By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/21 12:01:29 by jaubry--          #+#    #+#              #
-#    Updated: 2025/08/06 09:16:49 by jaubry--         ###   ########lyon.fr    #
+#    Updated: 2025/08/07 03:36:23 by jaubry--         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,6 @@ UTILS_SRCS		= color_utils.c \
 				  file_utils.c \
 				  math_utils.c quad_bezier_utils.c \
 				  scale_utils.c transform_utils.c \
-				  time_utils.c \
 				  error_handler.c
 UTILS_SRCS		:= $(addprefix $(UTILS_DIR)/, $(UTILS_SRCS))
 
