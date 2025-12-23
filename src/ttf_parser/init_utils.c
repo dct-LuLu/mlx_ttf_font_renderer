3e40eb62fc026ce819ec75b86c618913302fccd2
jaubry-- <jaubry--@student.42lyon.fr>
2025-12-23 21:11:24 +0100

    FIX: compilation erorr misisng return result for init subtable offset function

--- src/ttf_parser/init_utils.c
+++ src/ttf_parser/init_utils.c
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 10:03:36 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/12/23 20:59:11 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/12/23 21:10:12 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ static inline int	init_offset_subtable(t_ttf_font *font)
 		register_complex_err_msg(FRDR_E_MSG_ALLOC, "subtable offsets");
 		return (error(pack_err(FRDR_ID, FRDR_E_ALLOC), FL, LN, FC));
 	}
+	return (0);
 }
 
 static int	init_subtables(t_ttf_font *font)
@@ -52,7 +53,7 @@ int	init_ttf_struct(t_ttf_font **font)
 	*font = ft_calloc(sizeof(t_ttf_font), 1);
 	if (!*font)
 		return (-1);
-	if ((init_offset_subtables(*font) != 0) || (init_subtables(*font) != 0))
+	if ((init_offset_subtable(*font) != 0) || (init_subtables(*font) != 0))
 		return (error(pack_err(FRDR_ID, FRDR_E_SUBSTRUCT), FL, LN, FC));
 	return (0);
 }
